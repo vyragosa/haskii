e5e836e26c72ea8bb489d6d68b63fd15925d8598
Dmitriy Fonin <flower.daf29@gmail.com>
2026-03-04 20:28:56 +0300

    Add code for image convertion

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,43 @@
+-- | Точка входа приложения hascii.
 module Main (main) where
+
+import Ascii (Options (..), imageToAscii)
+import Options.Applicative
+import System.Exit (exitFailure)
+
+-- | Парсер аргументов командной строки.
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> argument str
+        ( metavar "IMAGE"
+       <> help "Путь к входному изображению (PNG, JPG, BMP)" )
+  <*> option auto
+        ( long "width"
+       <> short 'w'
+       <> metavar "INT"
+       <> value 80
+       <> showDefault
+       <> help "Ширина ASCII-арта в символах" )
+  <*> switch
+        ( long "invert"
+       <> short 'i'
+       <> help "Инвертировать яркость (светлое ↔ тёмное)" )
+  <*> switch
+        ( long "dense"
+       <> short 'd'
+       <> help "Использовать плотную (расширенную) палитру" )
+
+-- | Информация о программе для --help.
+programInfo :: ParserInfo Options
+programInfo = info (optionsParser <**> helper)
+  ( fullDesc
+ <> progDesc "Преобразует изображение IMAGE в ASCII-арт"
+ <> header   "hascii — генератор ASCII-арта из изображений" )
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  opts   <- execParser programInfo
+  result <- imageToAscii opts
+  case result of
+    Left  err -> putStrLn err >> exitFailure
+    Right art -> putStr art
